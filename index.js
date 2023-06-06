facaeb0bdab20cadeb0d765ba0cb7c170a4b881c
Michael Barrett <mikeyomam3@gmail.com>
2023-06-06 13:57:26 -0400

    Removed mustache
    Mustache doesn't want to work.

--- index.js
+++ index.js
@@ -1,11 +1,6 @@
 
 window.onload = () => {
 
-    function renderHello() {
-        const template = document.getElementById('template').innerHTML;
-        const rendered = Mustache.render(template, { name: 'Luke' });
-        document.getElementById('target').innerHTML = rendered;
-      }
     /**
      * Just a test function
      */
