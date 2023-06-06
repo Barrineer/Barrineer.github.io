11b3a0829d9d42d6ec9e07b77dd1136939ba3523
Michael Barrett <mikeyomam3@gmail.com>
2023-06-06 10:25:26 -0400

    Added 404 Page
    Added a custom 404 page which shows the url.

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
+//index js
+
 function changeText(){
     document.getElementById("b1").innerText = "Bye";
+}
+
+//404 js
+var url_404 = document.getElementById("404_url");
+
+if(url_404){
+    url_404.innerHTML = window.location.href;
 }
