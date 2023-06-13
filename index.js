202c062b43153f904020121ce3f07185da4fb557
Michael Barrett <mikeyomam3@gmail.com>
2023-06-13 14:07:31 -0400

    Added js for footer to appear

--- index.js
+++ index.js
@@ -1,37 +1,27 @@
 
 window.onload = () => {
 
-    function renderHello() {
-        const template = document.getElementById('template').innerHTML;
-        const rendered = Mustache.render(template, { name: 'Luke' });
-        document.getElementById('target').innerHTML = rendered;
-      }
-    /**
-     * Just a test function
-     */
-    function changeText(){
-        document.getElementById("b1").innerText = "Bye";
-    }
-
     //current_url: Displays the current page's full url
     var current_url = document.getElementById("current_url");
     var render_header = document.getElementById("main_header");
-    var render = document.getElementById('template');
+    var render_footer = document.getElementById('main_footer');
 
     if(current_url){
         current_url.innerHTML = window.location.href;
-    }
-
-    if(render){
-        renderHello();
     }
 
     if(render_header){
         fetch("src/html/header.html").then((response) => response.text()).then((template) => {
             const rendered_header = Mustache.render(template);
             render_header.outerHTML = rendered_header;
-        })
-        
+        });
+    }
+
+    if(render_footer){
+        fetch("src/html/footer.html").then((response) => response.text()).then((template) => {
+            const rendered_footer = Mustache.render(template);
+            render_footer.outerHTML = rendered_footer;
+        });
     }
 
 
