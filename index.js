ff2cd2bad17548cb8b51291bde0436353811c174
Michael Barrett <74513436+Barrineer@users.noreply.github.com>
2024-04-10 13:02:13 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,30 +1,37 @@
 window.onload = () => {
 
     let data = "{username=helloworld}";
-    let url = "https://script.google.com/macros/s/AKfycbzw7uxapqtbVNuTi360p9aN8DCjF8o8CtweU04B68yYe-KdPWSh84Tgrl4EDwZoI2qN/exec";
+    let post_url = "https://script.google.com/macros/s/AKfycbzw7uxapqtbVNuTi360p9aN8DCjF8o8CtweU04B68yYe-KdPWSh84Tgrl4EDwZoI2qN/exec";
+    let get_url = post_url + "?username=helloworld";
 
     //We want post requests
     async function postData(data){
 
         try{
-            const resp = await fetch(url, {
+            const resp = await fetch(post_url, {
                 method:"POST",
-                redirect:"follow",
-                headers:{
-                    "Content-Type": "text/plain;charset=utf-8",
-                    "Access-Control-Allow-Origin": "*",
-                },
                 body: JSON.stringify(data),
             });
 
             const result = await resp.json;
             console.log("Success!",result)
         } catch(e){
-            console.log(`Error posting data to ${url}`,e)
+            console.log(`Error posting data to ${post_url}`,e)
         }
 
     }
 
+    async function getData(){
+        try{
+            const res = await fetch(get_url);
+            const result = await res.json;
+            console.log("Success",result);
+        } catch(e){
+            console.log("Error getting data",e);
+        }
+        
+    }
     postData(data);
+    //getData();
 
 }
