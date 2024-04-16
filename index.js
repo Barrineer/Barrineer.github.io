0e6fc0eb35dee5f5e0464d42dd225fbdf984176e
Michael Barrett <74513436+Barrineer@users.noreply.github.com>
2024-04-15 21:16:58 -0400

    Update index.js
    Updated Apps Script Code. Changed url to new version.

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
 window.onload = () => {
 
     let data = "{username=helloworld}";
-    let post_url = "https://script.google.com/macros/s/AKfycbzw7uxapqtbVNuTi360p9aN8DCjF8o8CtweU04B68yYe-KdPWSh84Tgrl4EDwZoI2qN/exec";
+    let post_url = "https://script.google.com/macros/s/AKfycbyf6mVEpxdzKgreGXlH3pYCvv5MHPU8swIKYeIA211sNcVTi_i18QsPYffANtZIHEOk/exec"; //V3
     let get_url = post_url + "?username=helloworld";
+
+    /* Comparison - Apps Script VS MongoDB
+    ___________Apps Script______________ (Slow, slighly)
+    - More secure since urls cannot be directly accessed that are used for processing data. No visibility of access keys.
+    - slower load time
+    - About 600k requests per month
+    - Not directly connected to the db, may be slower fetching data
+    - Each function can run for at most 6 minutes.
+    - Can set triggers to preload longer functions and cache that data.
+
+    ___________MongoDB________________
+    - NOT SECURE. You NEED to send an authorization key no matter what, thus it would be forced to be put here and that would NOT be secure.
+    - Closer to db and has faster load times
+    - 1 Million requests per month, but they count for both db r/w and function calls
+
+    Will continue using Apps Script for security. Might use functions in conjuction with Apps Script for faster execution.
+    */
 
     //We want post requests
     async function postData(data){
@@ -31,7 +48,7 @@ window.onload = () => {
         }
         
     }
+    
     postData(data);
     //getData();
-
 }
