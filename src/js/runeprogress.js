fd2be68fe0cff95c2deb946e910f138a1d481edb
Michael Barrett <74513436+Barrineer@users.noreply.github.com>
2025-04-15 21:41:36 -0400

    failure due to cors error
    Good luck doing this project without a backend server to make the api calls. Apps Script may be possible but extremely slow.

--- src/js/runeprogress.js
+++ src/js/runeprogress.js
@@ -2,11 +2,15 @@ window.onload = () => {
 
     let data = "{player=Giron Pecur}";
     let get_url = "https://secure.runescape.com/m=itemdb_rs/api/catalogue/category.json?category=9";
+    let header_data = "{username=helloworld}"
 
     //We want post requests
     async function getData(){
         try{
-            const res = await fetch(get_url);
+            const res = await fetch(get_url, {
+                method:"GET",
+                headers:JSON.stringify(header_data),
+            });
             const result = await res.json;
             console.log("Success",result);
         } catch(e){
@@ -15,6 +19,11 @@ window.onload = () => {
         
     }
 
-    getData();
+    //getData();
+
+    //Final consensus:
+    //It is impossible to only use GitHub Pages as a website for grabbing api calls. You need some sort of backend server to make the api calls and serve them onto a website.
+    //The only use for GitHub Pages is for creating static pages ONLY. CORS prevents any api calls using pure js on the frontend unless the server the data it is requesting it 
+    //from allows the resource to be shared (unless its your own server, its most likely not allowed).
 
 }
